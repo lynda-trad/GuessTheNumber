c1f18a8974a3bf4b36464ea69b024f86d2ba15a3
lynda-trad <91526400+lynda-trad@users.noreply.github.com>
2022-11-16 11:07:37 +0100

    GuessTheNumber game working properly on console

---
+++ GuessTheNumber.java
@@ -0,0 +1,42 @@
+package guessTheNumber;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class GuessTheNumber
+{
+	public static void main(String[] args) throws IOException
+	{
+		int numberToGuess = (int)Math.floor(Math.random()*(101)+1);
+		int numberGuessed = 0;
+		System.out.println("~ Guess The Number ! ~");
+		System.out.println("~ Please enter a number between 1 and 100. ~");
+		while(numberGuessed != numberToGuess)
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+            String numberGuessedStr = br.readLine();
+            try 
+            {
+            	numberGuessed = Integer.parseInt(numberGuessedStr);
+            	if(numberGuessed == numberToGuess)
+            	{
+            		System.out.println("~ Congratulations ! The number was indeed " + numberGuessed + " !~");
+        	        br.close();
+            		System.exit(0);
+            	}
+            	else if(numberGuessed < numberToGuess)
+            	{
+            		System.out.println("~ The number to guess is HIGHER than " + numberGuessed + " ! ~");
+            	}
+            	else if (numberGuessed > numberToGuess)
+            	{
+            		System.out.println("~ The number to guess is LOWER than " + numberGuessed + " ! ~");
+            	}
+            } catch (NumberFormatException e) 
+            {
+                System.out.println("Please enter an number.");
+            }
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+-- Guess The Number ! --
+
+The program will randomly choose a number between 1 and 100.
+
+Enter a number with your keyboard, press Enter and the program will tell you if the number you guessed is higher or lower than the number you're looking for !
